ab8e54b224815d87f5353a40afc89af5a82ab91e
“kevin-99-oop” <kevinpandya18@gnu.ac.in>
2023-12-21 11:28:32 +0530

    css_load

--- rapturetech/settings.py
+++ rapturetech/settings.py
@@ -147,6 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
