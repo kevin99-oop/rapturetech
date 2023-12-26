ba04be6f157e3fc1f732820d536eb8ff0503cf20
“kevin-99-oop” <kevinpandya18@gnu.ac.in>
2023-12-26 16:56:52 +0530

    change_drec

--- apps/api/models.py
+++ apps/api/models.py
@@ -5,8 +5,35 @@ class CtRecord(models.Model):
     field_name = models.CharField(max_length=255)
 
 class DRecord(models.Model):
-    # Add fields specific to DRecord model
-    field_name = models.CharField(max_length=255)
+    REC_TYPE = models.CharField(max_length=1)
+    SLIP_TYPE = models.CharField(max_length=1)
+    ST_ID = models.CharField(max_length=255)
+    CUST_ID = models.IntegerField()
+    TotalFileRecord = models.IntegerField()
+    FlagEdited = models.CharField(max_length=1)
+    MType = models.CharField(max_length=1)
+    RecordingDate = models.DateField()
+    SHIFT = models.CharField(max_length=1)
+    FAT = models.FloatField()
+    FAT_UNIT = models.CharField(max_length=1)
+    SNF = models.FloatField()
+    SNF_UNIT = models.CharField(max_length=1)
+    CLR = models.FloatField()
+    CLR_UNIT = models.CharField(max_length=1)
+    WATER = models.FloatField()
+    WATER_UNIT = models.CharField(max_length=1)
+    QT = models.FloatField()
+    QT_UNIT = models.CharField(max_length=1)
+    RATE = models.FloatField()
+    Amount = models.FloatField()
+    CAmount = models.FloatField()
+    CSR_NO = models.IntegerField()
+    CREV = models.IntegerField()
+    END_TAG = models.CharField(max_length=1)
+    dpuid = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.REC_TYPE} - {self.ST_ID} - {self.RecordingDate}"
 
 class DpuAsKcs(models.Model):
     # Add fields specific to DpuAsKcs model
