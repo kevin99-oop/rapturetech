0335610b97dd36719d3212e4cf24939ee6b54de5
“kevin-99-oop” <kevinpandya18@gnu.ac.in>
2023-12-28 17:20:04 +0530

    drec_views

--- apps/common/views.py
+++ apps/common/views.py
@@ -266,3 +266,13 @@ def list_drec(request):
 class DRECListCreateView(generics.ListCreateAPIView):
     queryset = DREC.objects.all()
     serializer_class = DRECSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        serializer.save()
