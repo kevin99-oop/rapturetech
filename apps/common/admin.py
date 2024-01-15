07ff7883203c941d294558a9894d6736d4086668
“kevin-99-oop” <kevinpandya18@gnu.ac.in>
2024-01-16 00:07:28 +0530

    drec_rechange_1

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -8,7 +8,7 @@ class DPUAdmin(admin.ModelAdmin):
     list_display = ('user', 'location', 'dpu_id', 'society', 'mobile_number', 'owner', 'status', 'Amount', 'CAmount')
 @admin.register(DREC)
 class DRECAdmin(admin.ModelAdmin):
-    list_display = ('REC_TYPE', 'SLIP_TYPE', 'ST_ID', 'CUST_ID', 'RecordingDate', 'SHIFT', 'QT', 'Amount', 'CAmount','dpuid')
+    list_display = ('REC_TYPE', 'SLIP_TYPE', 'ST_ID', 'CUST_ID', 'RecordingDate', 'SHIFT', 'QT', 'Amount', 'CAmount','dpu')
     search_fields = ['ST_ID', 'CUST_ID']  # Add fields you want to search on
 
     # Additional configurations as needed
