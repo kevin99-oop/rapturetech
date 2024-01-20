658c85f623f6da305eecf7b57f19970b1c9dc3a0
“kevin-99-oop” <kevinpandya18@gnu.ac.in>
2024-01-21 03:48:36 +0530

    cidrange_edit

--- apps/common/models.py
+++ apps/common/models.py
@@ -62,7 +62,7 @@ class DREC(models.Model):
 
 class Customer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    st_id = models.CharField(max_length=255)
+    st_id = models.CharField(max_length=50)
     csv_file = models.FileField(upload_to='csv_files/')
     start_range = models.IntegerField()
     end_range = models.IntegerField()
