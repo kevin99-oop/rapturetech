e3ab774422a55b6ca1819ef269a1451e2bc2fab3
“kevin-99-oop” <kevinpandya18@gnu.ac.in>
2024-03-21 14:09:13 +0530

    use RDS database

--- rapturetech/local_settings.py
+++ rapturetech/local_settings.py
@@ -9,20 +9,20 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Database settings for the project
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Database engine (SQLite in this case)
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Database file path
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',  # Database engine (SQLite in this case)
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Database file path
+#     }
+# }
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'rapturetechdb',
-#        'USER': 'rapturetech_mas',
-#        'PASSWORD': 'Raptur3t3chN0S3cPass',
-#        'HOST': 'db-instance-rapturetech.c7msy0kga8dr.ap-south-1.rds.amazonaws.com',
-#        'PORT': '5432'
-#    }
-#}
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'rapturetechdb',
+       'USER': 'rapturetech_mas',
+       'PASSWORD': 'Raptur3t3chN0S3cPass',
+       'HOST': 'db-instance-rapturetech.c7msy0kga8dr.ap-south-1.rds.amazonaws.com',
+       'PORT': '5432'
+   }
+}
