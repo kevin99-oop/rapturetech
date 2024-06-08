ac1ea686e1e03466d2623315b6ca87240cbeecdb
“kevin-99-oop” <kevinpandya18@gnu.ac.in>
2024-06-08 23:44:53 +0530

    database change to production

--- rapturetech/local_settings.py
+++ rapturetech/local_settings.py
@@ -8,36 +8,36 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Database settings for the project
+# # Database settings for the project
 
-#for dev we need to set env variable 
-#for production (export ENV=prod)
-print("Current Env:- ",os.getenv("ENV","dev"))
-if os.getenv("ENV","dev")=="dev":
-    print("dev database selected")
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Database engine (SQLite in this case)
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Database file path
-    }
-}
-else:
-    print("Production database selected")
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Database engine (SQLite in this case)
-        'NAME': os.path.join(BASE_DIR, 'production.sqlite3'),  # Database file path
-    }
-}
-
-
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'rapturedb',
-#        'USER': 'sgnonsuser',
-#        'PASSWORD': 'SgnRapDbPas01',
-#        'HOST': '13.235.128.158',
-#        'PORT': '5432'
-#    }
+# #for dev we need to set env variable 
+# #for production (export ENV=prod)
+# print("Current Env:- ",os.getenv("ENV","dev"))
+# if os.getenv("ENV","dev")=="dev":
+#     print("dev database selected")
+#     DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',  # Database engine (SQLite in this case)
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Database file path
+#     }
 # }
+# else:
+#     print("Production database selected")
+#     DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',  # Database engine (SQLite in this case)
+#         'NAME': os.path.join(BASE_DIR, 'production.sqlite3'),  # Database file path
+#     }
+# }
+
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'rapturedb',
+       'USER': 'sgnonsuser',
+       'PASSWORD': 'SgnRapDbPas01',
+       'HOST': '13.235.128.158',
+       'PORT': '5432'
+   }
+}
